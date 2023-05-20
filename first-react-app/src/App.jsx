0bb630005f957620aba2600d24a7d3c8ee4703e8
jpared3s <jparedes.wall@gmail.com>
2023-05-20 08:38:40 -0400

    dynamic component styles exercise

--- first-react-app/src/App.jsx
+++ first-react-app/src/App.jsx
@@ -2,10 +2,12 @@ import "./App.css";
 // import Greeter from "./greeter";
 // import Die from "./die";
 import DoubleDice from "./DoubleDice";
+import Heading from "./Heading";
 
 function App() {
   return (
     <div>
+      <Heading color= "red" text = "Raptors"></Heading>
       {/* <Greeter person="Julian" from="Jacob"/>
       <Die numSides={20}/> */}
       <DoubleDice />
--- first-react-app/src/DoubleDice.jsx
+++ first-react-app/src/DoubleDice.jsx
@@ -1,9 +1,10 @@
 export default function DoubleDice() {
   const num1 = Math.floor(Math.random() * 3) + 1;
   const num2 = Math.floor(Math.random() * 3) + 1;
+  const styles = {color: num1 === num2 ? "green" : "red"}
 
   return (
-    <div>
+    <div className="DoubleDice" style={styles}>
       <h2>Double Dice</h2>
       {num1 === num2 && <h3>You Win!</h3>}
       <p>Num 1: {num1}</p>
