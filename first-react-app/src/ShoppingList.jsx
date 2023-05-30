bb8c72b6a27e7761cd98e72de8cb7c1b23ee2989
jpared3s <jparedes.wall@gmail.com>
2023-05-29 20:04:39 -0400

    shopping list part 1

--- first-react-app/src/ShoppingList.jsx
+++ first-react-app/src/ShoppingList.jsx
@@ -4,10 +4,14 @@ export default function ShoppingList({ items }) {
   return (
     <ul>
       {items.map((i) => (
-        <li style={{ color: i.completed ? "grey" : "red" , textDecoration: "line-through"}}>
+        <li
+          style={{
+            color: i.completed ? "grey" : "red",
+            textDecoration: i.completed ? "line-through" : "none",
+          }}
+        >
           {i.item} - {i.quantity}
         </li>
       ))}
-    </ul>
-  );
+    </ul>  );
 }
